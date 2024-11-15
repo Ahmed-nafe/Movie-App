d83b9d08d204f6e185ea5bd26ea89ba03f26afb5
Ahmed Nafe <abonafe27@gmail.com>
2024-11-15 15:03:09 +0200

    Finish Home Tab

--- lib/features/presentation/screens/home_tab/views/home_tab_body.dart
+++ lib/features/presentation/screens/home_tab/views/home_tab_body.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:movie/features/presentation/screens/home_tab/views/recommend_section.dart';
+import 'new_releases_section.dart';
 
 class HomeTabBody extends StatelessWidget {
   const HomeTabBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  const Column(
-      children: [
-      ],
+    return SafeArea(
+      child: Column(
+        children: [
+          Image.asset(
+            fit: BoxFit.fill,
+            "assets/Image.png",
+            width: double.infinity,
+            height: MediaQuery.sizeOf(context).height * 0.28,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(
+              top: 90,
+              bottom: 30,
+            ),
+            child: NewReleasesSection(),
+          ),
+          const RecommendSection()
+        ],
+      ),
     );
   }
 }
