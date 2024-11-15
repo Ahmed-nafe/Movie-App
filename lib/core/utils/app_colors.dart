2efa972faca532f8cfbf7d38438a796be3e4da48
Ahmed Nafe <abonafe27@gmail.com>
2024-11-15 15:02:55 +0200

    Finish Recommend Section

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -4,4 +4,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static  const Color backgroundColor = Colors.black;
   static  const Color primaryColor = Color(0xffFFBB3B);
+  static  const Color shadeGray = Color(0xff282A28);
 }
---
+++ lib/features/presentation/screens/home_tab/views/recommend_section.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:movie/features/presentation/screens/home_tab/views/recommend_list_view.dart';
+import '../../../../../core/utils/app_colors.dart';
+
+class RecommendSection extends StatelessWidget {
+  const RecommendSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        width: double.infinity,
+        margin: const EdgeInsets.only(bottom: 25),
+        decoration: const BoxDecoration(
+          color: AppColors.shadeGray,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 19.0, top: 14, bottom: 7),
+              child: Text(
+                "Recommended",
+                style: GoogleFonts.montserrat(
+                  fontSize: 19,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const RecommendListView(),
+          ],
+        ),
+      ),
+    );
+  }
+}
