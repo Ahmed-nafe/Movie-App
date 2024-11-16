c1960ecb7db12678c5fd90af90554a21b60cc4d0
Ahmed Nafe <abonafe27@gmail.com>
2024-11-16 16:38:47 +0200

    Finish Details Page

---
+++ lib/features/presentation/screens/details_screen/views/film_details_body.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:movie/core/utils/app_colors.dart';
+
+import 'film_details.dart';
+
+class FilmDetailsBody extends StatelessWidget {
+  const FilmDetailsBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: AppColors.backgroundColor,
+        appBar: AppBar(
+          backgroundColor: AppColors.shadeGray,
+          foregroundColor: Colors.white,
+          title: Text(
+            "Dora and the lost city of gold",
+            style:
+                GoogleFonts.openSans(fontSize: 20, fontWeight: FontWeight.w600),
+          ),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: MediaQuery.sizeOf(context).height * 0.25,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/Image.png"), fit: BoxFit.cover),
+              ),
+            ),
+            const FilmDetails(),
+          ],
+        ),
+      ),
+    );
+  }
+}
