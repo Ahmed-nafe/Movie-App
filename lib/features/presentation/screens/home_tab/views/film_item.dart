ad9f2d2493800bb29ea4e903f0e400ace208a64f
Ahmed Nafe <abonafe27@gmail.com>
2024-11-16 17:25:56 +0200

    Add Top Section in Home Page

---
+++ lib/features/presentation/screens/home_tab/views/film_item.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FilmItem extends StatelessWidget {
+  const FilmItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          height: MediaQuery.sizeOf(context).height * 0.22,
+          width: MediaQuery.sizeOf(context).width * 0.34,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            image: const DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('assets/Image.png'),
+            ),
+          ),
+        ),
+        const SizedBox(width: 7),
+        Container(
+          padding: EdgeInsets.only(
+            top: MediaQuery.sizeOf(context).height * 0.12,
+            left: MediaQuery.sizeOf(context).height * 0.004,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                "Dora and the lost city of gold",
+                style: GoogleFonts.openSans(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 6),
+              Text(
+                "2019  PG-13  2h 7m",
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white.withOpacity(0.7),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/features/presentation/screens/home_tab/views/home_tab_body.dart
+++ lib/features/presentation/screens/home_tab/views/home_tab_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movie/features/presentation/screens/home_tab/views/recommend_section.dart';
+import 'package:movie/features/presentation/screens/home_tab/views/top_section_item.dart';
 import 'new_releases_section.dart';
 
 class HomeTabBody extends StatelessWidget {
@@ -7,31 +8,18 @@ class HomeTabBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: Column(
         children: [
-          Stack(
-            children: [
-              Container(
-                height: MediaQuery.sizeOf(context).height * 0.28,
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage('assets/Image.png'),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          const Padding(
+          TopSectionItem(),
+          Padding(
             padding: EdgeInsets.only(
-              top: 90,
-              bottom: 30,
+              top: 20,
+              bottom: 16,
             ),
             child: NewReleasesSection(),
           ),
-          const RecommendSection(),
+          RecommendSection(),
         ],
       ),
     );
