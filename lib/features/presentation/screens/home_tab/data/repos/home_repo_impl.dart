f95b90a9c252262b3485b2f4302584b51c53d65b
Ahmed Nafe <abonafe27@gmail.com>
2024-11-17 14:28:18 +0200

    Create Home Repo

---
+++ lib/features/presentation/screens/home_tab/data/repos/home_repo_impl.dart
@@ -0,0 +1,32 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:movie/core/utils/api_manger.dart';
+import 'package:movie/core/utils/errors.dart';
+import '../model/NewReleaseModel.dart';
+import 'package:movie/features/presentation/screens/home_tab/data/repos/home_repo.dart';
+
+class HomeRepoImplement implements HomeRepo {
+  ApiManger apiManger;
+
+  HomeRepoImplement(
+    this.apiManger,
+  );
+
+  @override
+  Future<Either<Failure, NewReleaseModel>> fetchNewReleases() async {
+    try {
+      var response = await apiManger.get(
+          endPoint: "upcoming?api_key=0403d62457b0709fa04b96044b5aa966");
+      NewReleaseModel newReleasesModel = NewReleaseModel.fromJson(response);
+      return right(newReleasesModel);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(
+          ServerFailure(e.toString()),
+        );
+      }
+    }
+  }
+}
