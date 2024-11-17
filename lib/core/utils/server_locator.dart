3617c184eb608b1e171fadac36276cf2c0ea0666
Ahmed Nafe <abonafe27@gmail.com>
2024-11-17 14:28:51 +0200

    Create Server Locator

---
+++ lib/core/utils/server_locator.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:movie/features/presentation/screens/home_tab/data/repos/home_repo_impl.dart';
+import 'api_manger.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<ApiManger>(ApiManger(Dio()));
+  getIt.registerSingleton<HomeRepoImplement>(
+    HomeRepoImplement(
+      getIt.get<ApiManger>(),
+    ),
+  );
+  // getIt.registerSingleton<SearchRepoImpl>(
+  //   SearchRepoImpl(
+  //     getIt.get<ApiManger>(),
+  //   ),
+  // );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie/core/utils/server_locator.dart';
+import 'package:movie/features/presentation/screens/home_tab/data/repos/home_repo_impl.dart';
+import 'package:movie/features/presentation/screens/home_tab/manger/new_releses_cubit/new_releases_cubit.dart';
 import 'core/utils/app_router.dart';
 
-
 void main() {
+  setup();
   runApp(const MyApp());
 }
 
@@ -12,9 +15,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: AppRouter.router,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => NewReleasesCubit(
+            getIt.get<HomeRepoImplement>(),
+          )..fetchNewReleases(),
+        ),
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerConfig: AppRouter.router,
+      ),
     );
   }
 }
