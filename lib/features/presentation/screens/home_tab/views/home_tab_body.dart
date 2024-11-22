d23d2a0f560d2f26a217582c2c73712854cb38e3
Ahmed Nafe <abonafe27@gmail.com>
2024-11-22 17:42:46 +0200

    Add Refresh Page To Load Data

--- lib/features/presentation/screens/home_tab/views/home_tab_body.dart
+++ lib/features/presentation/screens/home_tab/views/home_tab_body.dart
@@ -1,25 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie/features/presentation/screens/home_tab/manger/new_releses_cubit/new_releases_cubit.dart';
+import 'package:movie/features/presentation/screens/home_tab/manger/popular_cuibt/popular_cubit.dart';
+import 'package:movie/features/presentation/screens/home_tab/manger/recommended_cubit/recommended_cubit.dart';
 import 'package:movie/features/presentation/screens/home_tab/views/recommend_section.dart';
 import 'package:movie/features/presentation/screens/home_tab/views/top_section_item.dart';
 import 'new_releases_section.dart';
 
-class HomeTabBody extends StatelessWidget {
+class HomeTabBody extends StatefulWidget {
   const HomeTabBody({super.key});
 
   @override
+  State<HomeTabBody> createState() => _HomeTabBodyState();
+}
+
+class _HomeTabBodyState extends State<HomeTabBody> {
+  Future<void> _refreshPage() async {
+    context.read<PopularCubit>().fetchPopularMovies();
+    context.read<NewReleasesCubit>().fetchNewReleases();
+    context.read<RecommendedCubit>().fetchRecommended();
+    await Future.delayed(const Duration(seconds: 2)); // محاكاة انتظار البيانات
+    // print("Page refreshed");
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return RefreshIndicator(
+      color: Colors.white,
+      backgroundColor: Colors.blue,
+      onRefresh: _refreshPage,
       child: Column(
         children: [
-          TopSectionItem(),
-          Padding(
-            padding: EdgeInsets.only(
-              top: 20,
-              bottom: 16,
+          Expanded(
+            child: ListView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              children: const [
+                TopSectionItem(),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 20,
+                    bottom: 16,
+                  ),
+                  child: NewReleasesSection(),
+                ),
+                RecommendSection(),
+              ],
             ),
-            child: NewReleasesSection(),
           ),
-          RecommendSection(),
         ],
       ),
     );
